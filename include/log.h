0685d304e3afd91a88c82d233af8d4bda4e2da25
airgalss <2380153794@qq.com>
2024-03-01 17:07:01 +0800

    clean

--- include/log.h
+++ include/log.h
@@ -1,10 +1,3 @@
-/**
- * Copyright (c) 2020 rxi
- *
- * This library is free software; you can redistribute it and/or modify it
- * under the terms of the MIT license. See `log.c` for details.
- */
-
 #ifndef LOG_H
 #define LOG_H
 
